bb73a613595ded2f16c05e4866249666661b3141
IrynaShvets <86491334+IrynaShvets@users.noreply.github.com>
2022-12-23 21:24:20 +0200

    added changes ten function

--- ten.js
+++ ten.js
@@ -1,13 +1,20 @@
-function spam(str) {
-  if (Array.isArray(str)) {
-    return str.some((el) => str.toLowerCase().indexOf(el.toLowerCase()) > -1);
-  }
+function checkingStringForSpam(str) {
+const arr = str.split(" ")
 
-  return str.indexOf(str) > -1;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+   if (arr[i].toLowerCase().includes(arr[j].toLowerCase())) {
+        return true; 
+      }
+  }
+}
+return false;
 }
 
-console.log(spam("которая проверяет строку на спам строку на спам"));
-console.log(spam("которая проверяет строку"));
+console.log(checkingStringForSpam("которая проверяет строку на спам строку на спам"));
+console.log(checkingStringForSpam("которая проверяет строку"));
+console.log(checkingStringForSpam("Привіт, де ти"));
+console.log(checkingStringForSpam("Привіт, привіт"));
 
 /* 
 10. Напишите функцию, которая проверяет строку на спам. 
